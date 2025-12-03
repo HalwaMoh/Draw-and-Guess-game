3f88ce0c0e9d03aa53de4a746c837b93488d6ae6
HanaGadissa <Hanagadissa09@gmail.com>
2025-12-03 08:34:08 +0300

    Added class player as one of game controller

---
+++ Player.java
@@ -0,0 +1,11 @@
+class Player {
+    // Attributes
+    private String name;
+    private int score;
+
+    // Methods
+    public String getName();
+    public int getScore();
+    // (WordSource interface unsure yet)
+     pickWord();
+}
